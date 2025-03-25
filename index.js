5e42eed0b4cea16a67ec14428e7b161390419a05
Akash <akash9086236324@gmail.com>
2025-03-25 13:20:05 +0530

    added vercel.json

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const PORT = process.env.PORT || 3000;
 async function start() {
     try {
 
+        console.log(`In start function with port ${PORT}`);
+
         await fastify.listen({ port: PORT, host: '0.0.0.0' });
         console.log(`Server running on port ${PORT}`);
     } catch (error) {
