39bf3934f6264a7f1d582e8c1368653012f07cbb
Akash <akash9086236324@gmail.com>
2025-03-25 16:08:07 +0530

    changed

--- mongo.js
+++ mongo.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+
+    if(!process.env.MONGO_URI){
+        console.log(`Mongo uri not loaded`);
+        process.exit(1);
+    }
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
